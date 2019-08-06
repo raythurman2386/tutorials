0104a727d7199c277d7e7d90664d09ce3ab80d80
raythurman2386 <raymondthurman5@gmail.com>
2019-08-06 19:53:59 -0400

    require the URL library

--- nodeJS/index.js
+++ nodeJS/index.js
@@ -5,10 +5,18 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
 // The server should respond to all requests with a string
 const server = http.createServer((req, res) => {
+  // Get the URL and parse it
+
+  // Get the PATH from the URL
+
+  // Send the response
   res.end('Hello World\n');
+
+  // Log the request path
 });
 
 // Start the server, and have it listen on port 3000
