47885e4d12fa2b5e6f14cced041cdc42643511c1
raythurman2386 <raymondthurman5@gmail.com>
2019-08-06 20:14:23 -0400

    parsed HTTP method

--- nodeJS/index.js
+++ nodeJS/index.js
@@ -16,11 +16,14 @@ const server = http.createServer((req, res) => {
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+  // Get the HTTP method
+  const method = req.method.toLowerCase();
+
   // Send the response
   res.end('Hello World\n');
 
   // Log the request path
-  console.log('Request received on path: ' + trimmedPath);
+  console.log(`Request received on path: ${trimmedPath} with method: ${method}`);
 });
 
 // Start the server, and have it listen on port 3000
