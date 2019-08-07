7f28f7771d3562a78d64c6f4d1182e62044ab88b
raythurman2386 <raymondthurman5@gmail.com>
2019-08-06 20:43:25 -0400

    getting the payload and using decoder to return string

--- nodeJS/index.js
+++ nodeJS/index.js
@@ -6,6 +6,7 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string
 const server = http.createServer((req, res) => {
@@ -25,11 +26,21 @@ const server = http.createServer((req, res) => {
   // Get the headers as an object
   const headers = req.headers;
 
-  // Send the response
-  res.end('Hello World\n');
+  // Get the payload, if any
+  const decoder = new StringDecoder('utf-8');
+  const buffer = '';
+  req.on('data', (data) => {
+    buffer += decoder.write(data);
+  });
+  req.on('end', () => {
+    buffer += decoder.end();
 
-  // Log the request path
-  console.log(`Request received with these headers: `, headers);
+    // Send the response
+    res.end('Hello World\n');
+
+    // Log the request path
+    console.log(`Request received with this payload: `, buffer);
+  });
 });
 
 // Start the server, and have it listen on port 3000
