cea1293e2fd5aad4190a4cd160413518bcbd5216
raythurman2386 <raymondthurman5@gmail.com>
2019-08-24 22:53:27 -0400

    imported Link from gatsby

--- gatsby-bootcamp/src/pages/index.js
+++ gatsby-bootcamp/src/pages/index.js
@@ -1,10 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const HomePage = () => {
   return (
     <div>
       <h1>Hello</h1>
       <h2>I'm Ray, a full stack developer living in Indiana.</h2>
+      <p>
+        Need a developer? <Link to="/contact">Contact me</Link>
+      </p>
     </div>
   )
 }
