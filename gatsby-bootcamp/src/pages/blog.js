da9d57d137ae7bad3581e64c6303335239d20f79
raythurman2386 <raymondthurman5@gmail.com>
2019-08-24 22:28:36 -0400

    updated index page

---
+++ gatsby-bootcamp/src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const BlogPage = () => {
+  return (
+    <div>
+      <h1>My Blog</h1>
+      <p>Posts will show here later on!!</p>
+    </div>
+  )
+}
+
+export default BlogPage
--- gatsby-bootcamp/src/pages/index.js
+++ gatsby-bootcamp/src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>I'm Ray, a full stack developer living in Indiana.</h2>
+    </div>
+  )
+}
+
+export default HomePage
